d06f7f7016c580a86d8976bf1b694626a8b9d24c
Yakov Damen <yakov.damen@gmail.com>
2023-08-28 16:59:35 +0300

    28-08-23

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ import express from 'express';
 import morgan from 'morgan';
 import router from './routes/routes.js';
 import { getDataFromAPI } from './controller/productController.js';
+import cors from 'cors';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(morgan('tiny'));
 app.use('/api', router);
